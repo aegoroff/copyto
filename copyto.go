355e5345268ca08ff075da3ca9ce3d3d2e39c64d
egoroff <egoroff@gmail.com>
2018-10-15 08:39:41 +0300

    Command line interface draft

--- copyto.go
+++ copyto.go
@@ -6,10 +6,23 @@ import (
 )
 
 type options struct {
-	Source  string `goptions:"-s, --source, description='Path to the source folder, to copy (sync) data from'"`
-	Target  string `goptions:"-t, --target, description='Path to the target folder, to copy (sync) data to'"`
-	Version bool   `goptions:"--version, description='Print version'"`
+	Version bool `goptions:"--version, description='Print version'"`
+
+	goptions.Verbs
+
+	// Using command line to set source and target
+	CmdLine struct {
+		Source string `goptions:"-s, --source, obligatory, description='Path to the source folder, to copy (sync) data from'"`
+		Target string `goptions:"-t, --target, obligatory, description='Path to the target folder, to copy (sync) data to'"`
+	} `goptions:"cmdline"`
+
+	// Using command line to set source and target
+	Config struct {
+		Path string `goptions:"-p, --path, obligatory, description='Path to configuration file'"`
+	} `goptions:"config"`
 }
+
+type command func(options) error
 
 func main() {
 	opt := options{}
@@ -21,7 +34,7 @@ func main() {
 		return
 	}
 
-	if err != nil {
+	if len(opt.Verbs) == 0 || err != nil {
 		goptions.PrintHelp()
 		return
 	}
