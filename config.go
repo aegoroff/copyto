37464d1cc6805189de73dee14e0b3edf2aaafdac
egoroff <egoroff@gmail.com>
2018-10-16 20:15:46 +0300

    Configuration file mode draft

--- config.go
+++ config.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"github.com/BurntSushi/toml"
+)
+
+type tomlConfig struct {
+	Title       string
+	Definitions map[string]definition
+}
+
+type definition struct {
+	Source string
+	Target string
+}
+
 func configcmd(opt options) error {
+	var config tomlConfig
+	if _, err := toml.DecodeFile(opt.Config.Path, &config); err != nil {
+		return err
+	}
+
+	for _, v := range config.Definitions {
+		coptyfiletree(v.Source, v.Target, opt.Verbose)
+	}
+
 	return nil
 }
