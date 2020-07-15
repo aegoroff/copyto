0279073fe7ae26a716ba36702dc13317ffbec6fd
egoroff <egoroff@gmail.com>
2020-07-15 09:15:22 +0300

    refactoring: logic simplified. Instead of creating two trees we create one target tree and copy corresponding source file to it if present

--- logic/filetree.go
+++ logic/filetree.go
@@ -1,49 +1,23 @@
 package logic
 
-import (
-	"github.com/aegoroff/godatastruct/rbtree"
-)
-
-type fileTreeNode struct {
-	name  string
-	paths []string
+type file struct {
+	name string
 }
 
-func (x *fileTreeNode) LessThan(y interface{}) bool {
-	return less(x.String(), (y.(*fileTreeNode)).String())
+func (f *file) String() string {
+	return f.name
 }
 
-func (x *fileTreeNode) EqualTo(y interface{}) bool {
-	return equal(x.String(), (y.(*fileTreeNode)).String())
+func (f *file) LessThan(y interface{}) bool {
+	return less(f.String(), (y.(*file)).String())
 }
 
-func (x *fileTreeNode) String() string {
-	return x.name
+func (f *file) EqualTo(y interface{}) bool {
+	return equal(f.String(), (y.(*file)).String())
 }
 
-func addFileToTree(tree rbtree.RbTree, file string, path string) {
-	node := fileTreeNode{name: file}
-	found, ok := tree.Search(&node)
-
-	if ok {
-		addPathToFileNode(found.Key(), path)
-	} else {
-		addPathToFileNode(&node, path)
-
-		tree.Insert(&node)
-	}
-}
-
-func addPathToFileNode(n rbtree.Comparable, path string) {
-	key := n.(*fileTreeNode)
-	key.paths = append(key.paths, path)
-}
-
-func getFilePathsFromTree(tree rbtree.RbTree, file string) ([]string, bool) {
-	found, ok := tree.Search(&fileTreeNode{name: file})
-	if !ok {
-		return nil, false
-	}
-	key := found.Key().(*fileTreeNode)
-	return key.paths, true
+// newFile creates normalized (i.e. without base part) file node
+func newFile(base, path string) *file {
+	normalized := path[len(base):]
+	return &file{name: normalized}
 }
--- logic/logic.go
+++ logic/logic.go
@@ -16,38 +16,13 @@ type copyResult struct {
 	NotFoundInSource int64
 }
 
-type file struct {
-	name string
-}
-
-func (f *file) String() string {
-	return f.name
-}
-
-func (f *file) LessThan(y interface{}) bool {
-	return less(f.String(), (y.(*file)).String())
-}
-
-func (f *file) EqualTo(y interface{}) bool {
-	return equal(f.String(), (y.(*file)).String())
-}
-
-// newFile creates normalized (i.e. without base part) file node
-func newFile(base, path string) *file {
-	normalized := path[len(base):]
-	return &file{name: normalized}
-}
-
 // CopyFileTree does files tree coping
 func CopyFileTree(source, target string, filter Filter, fs afero.Fs, w io.Writer, verbose bool) {
+	ch := make(chan *string, 1024)
 
-	srcCh := make(chan *string, 1024)
-	tgtCh := make(chan *string, 1024)
+	go readDirectory(target, filter, fs, ch)
 
-	go readDirectory(source, filter, fs, srcCh)
-	go readDirectory(target, filter, fs, tgtCh)
-
-	res, missing := copyTree(srcCh, tgtCh, source, target, verbose, fs, w)
+	res, missing := copyTree(ch, source, target, verbose, fs, w)
 	printTotals(res, missing, w)
 }
 
@@ -71,47 +46,32 @@ func printTotals(res copyResult, missing []string, w io.Writer) {
 	_ = report.Execute(w, res)
 }
 
-func copyTree(sourceCh <-chan *string, targetCh <-chan *string, source string, target string, verbose bool, fs afero.Fs, w io.Writer) (copyResult, []string) {
-	sourcesTree, targetsTree := createTrees(sourceCh, targetCh)
+func copyTree(targetCh <-chan *string, source string, target string, verbose bool, fs afero.Fs, w io.Writer) (copyResult, []string) {
+	targetsTree := createTree(target, targetCh)
 
 	var result copyResult
 	var missing []string
 
-	if sourcesTree.Len() == 0 || targetsTree.Len() == 0 {
+	if targetsTree.Len() == 0 {
 		return result, missing
 	}
 
 	targetsTree.Ascend(func(n rbtree.Node) bool {
-		node := n.Key().(*fileTreeNode)
-		sources, ok := getFilePathsFromTree(sourcesTree, node.name)
-		srcFiles := rbtree.NewRbTree()
+		tgt := n.Key()
+		src := filepath.Join(source, tgt.String())
 
-		for _, src := range sources {
-			s := newFile(source, src)
-			srcFiles.Insert(s)
-		}
-
-		for _, tgt := range node.paths {
-			t := newFile(target, tgt)
-			if !ok {
-				result.NotFoundInSource++
-				missing = append(missing, t.String())
-				continue
+		_, err := fs.Stat(src)
+		if err == nil {
+			dst := filepath.Join(target, tgt.String())
+			if err := copyFile(src, dst, fs); err != nil {
+				log.Printf("%v", err)
+			} else if verbose {
+				_, _ = fmt.Fprintf(w, "[%s] copied to [%s]\n", src, dst)
 			}
-			_, ok := srcFiles.Search(t)
-
-			if ok {
-				src := filepath.Join(source, t.String())
-				if err := copyFile(src, tgt, fs); err != nil {
-					log.Printf("%v", err)
-				} else if verbose {
-					_, _ = fmt.Fprintf(w, "[%s] copied to [%s]\n", src, tgt)
-				}
-				result.TotalCopied++
-			} else {
-				result.NotFoundInSource++
-				missing = append(missing, t.String())
-			}
+			result.TotalCopied++
+		} else {
+			result.NotFoundInSource++
+			missing = append(missing, tgt.String())
 		}
 
 		return true
@@ -120,34 +80,13 @@ func copyTree(sourceCh <-chan *string, targetCh <-chan *string, source string, t
 	return result, missing
 }
 
-func createTrees(sourceCh <-chan *string, targetCh <-chan *string) (rbtree.RbTree, rbtree.RbTree) {
-	sourcesTree := rbtree.NewRbTree()
-	targetsTree := rbtree.NewRbTree()
-	srcDone := false
-	tgtDone := false
-	for {
-		if tgtDone && srcDone {
-			break
-		}
-
-		select {
-		case srcFile, ok := <-sourceCh:
-			srcDone = !ok
-			if ok {
-				p := *srcFile
-				file := filepath.Base(p)
-				addFileToTree(sourcesTree, file, p)
-			}
-		case tgtFile, ok := <-targetCh:
-			tgtDone = !ok
-			if ok {
-				p := *tgtFile
-				file := filepath.Base(p)
-				addFileToTree(targetsTree, file, p)
-			}
-		}
+func createTree(base string, ch <-chan *string) rbtree.RbTree {
+	res := rbtree.NewRbTree()
+	for f := range ch {
+		n := newFile(base, *f)
+		res.Insert(n)
 	}
-	return sourcesTree, targetsTree
+	return res
 }
 
 func readDirectory(dir string, filter Filter, fs afero.Fs, ch chan<- *string) {
