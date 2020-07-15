50c6b656335ecc09785322ae44d22e586d826a8d
egoroff <egoroff@gmail.com>
2020-07-15 22:08:43 +0300

    feature: concurrent directory reading implemented

--- logic/logic.go
+++ logic/logic.go
@@ -1,12 +1,12 @@
 package logic
 
 import (
+	"copyto/logic/internal/sys"
 	"fmt"
 	"github.com/aegoroff/godatastruct/rbtree"
 	"github.com/spf13/afero"
 	"io"
 	"log"
-	"os"
 	"path/filepath"
 	"text/template"
 )
@@ -18,16 +18,26 @@ type copyResult struct {
 
 // CopyFileTree does files tree coping
 func CopyFileTree(source, target string, filter Filter, fs afero.Fs, w io.Writer, verbose bool) {
-	ch := make(chan *string, 1024)
+	fileTree := rbtree.NewRbTree()
 
-	go readDirectory(target, filter, fs, ch)
+	sys.Scan(target, fs, func(f *sys.ScanEvent) {
+		if f.File == nil {
+			return
+		}
 
-	res, missing := copyTree(ch, source, target, verbose, fs, w)
+		if filter.Skip(f.File.Path) {
+			return
+		}
+
+		n := newFile(target, f.File.Path)
+		fileTree.Insert(n)
+	})
+
+	res, missing := copyTree(fileTree, source, target, verbose, fs, w)
 	printTotals(res, missing, w)
 }
 
 func printTotals(res copyResult, missing []string, w io.Writer) {
-
 	if len(missing) > 0 {
 		_, _ = fmt.Fprintf(w, "   Found files that present in target but missing in source:\n")
 	}
@@ -46,9 +56,7 @@ func printTotals(res copyResult, missing []string, w io.Writer) {
 	_ = report.Execute(w, res)
 }
 
-func copyTree(targetCh <-chan *string, source string, target string, verbose bool, fs afero.Fs, w io.Writer) (copyResult, []string) {
-	targetsTree := createTree(target, targetCh)
-
+func copyTree(targetsTree rbtree.RbTree, source string, target string, verbose bool, fs afero.Fs, w io.Writer) (copyResult, []string) {
 	var result copyResult
 	var missing []string
 
@@ -78,29 +86,4 @@ func copyTree(targetCh <-chan *string, source string, target string, verbose boo
 	})
 
 	return result, missing
-}
-
-func createTree(base string, ch <-chan *string) rbtree.RbTree {
-	res := rbtree.NewRbTree()
-	for f := range ch {
-		n := newFile(base, *f)
-		res.Insert(n)
-	}
-	return res
-}
-
-func readDirectory(dir string, filter Filter, fs afero.Fs, ch chan<- *string) {
-	walkDirBreadthFirst(dir, fs, func(parent string, entry os.FileInfo) {
-		if entry.IsDir() {
-			return
-		}
-
-		if filter.Skip(entry.Name()) {
-			return
-		}
-
-		path := filepath.Join(parent, entry.Name())
-		ch <- &path
-	})
-	close(ch)
 }
